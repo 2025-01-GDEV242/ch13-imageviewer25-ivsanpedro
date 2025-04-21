d4d437aeee167f524b86f3092e09caadac51951f
ivsanpedro <ivsanpedro2@gmail.com>
2025-04-21 10:05:42 -0400

    Create redchannel

---
+++ RedChannel.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+/**
+ * Write a description of class RedChannel here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RedChannel extends Filter
+{
+    /**
+     * Constructor for objects of class RedChannel Filter.
+     * @param name The name of the filter.
+     */
+    public RedChannel(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Apply this filter to an image.
+     * 
+     * @param  image  The image to be changed by this filter.
+     */
+    public void apply(OFImage image)
+    { 
+        int height = image.getHeight();
+        int width = image.getWidth();
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                Color pix = image.getPixel(x, y);
+                int redPixelVal = pix.getRed();
+                image.setPixel(x, y, new Color(redPixelVal, redPixelVal, redPixelVal));
+            }
+        }
+    }
+}
