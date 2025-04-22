0086ce3a9566499808f9ee83cf85e441065b1dd3
ivsanpedro <ivsanpedro2@gmail.com>
2025-04-21 20:13:45 -0400

    Create bluechannel and greenchannel

---
+++ BlueChannel.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+/**
+ * Write a description of class BlueChannel here.
+ *
+ * @author Ivana San Pedro
+ * @version 4/21/2025
+ */
+public class BlueChannel extends Filter
+{
+    /**
+     * Constructor for objects of class RedChannel Filter.
+     * @param name The name of the filter.
+     */
+    public BlueChannel(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Apply this filter to an image.
+     * 
+     * @param  image  The image to be changed by this filter.
+     */
+    public void apply(OFImage image)
+    { 
+        int height = image.getHeight();
+        int width = image.getWidth();
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                Color pix = image.getPixel(x, y);
+                int BluePixelVal = pix.getBlue();
+                image.setPixel(x, y, new Color(BluePixelVal, BluePixelVal, BluePixelVal));
+            }
+        }
+    }
+}
